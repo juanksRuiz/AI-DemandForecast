3a35f02d5b532b865c91001357aea53fc9a9082c
juanksRuiz <juankruizo10@gmail.com>
2025-05-22 10:54:59 -0500

    Ultimos cambios

--- 1_interface_demand_forecast.py
+++ 1_interface_demand_forecast.py
@@ -1,42 +1,45 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 from datetime import datetime
-import matplotlib.pyplot as plt
+import altair as alt
 
 from model.utility_funcs import *
 
 # ----- Streamlit App -----
-st.set_page_config(page_title="Predicción de Demanda", layout="wide")
+st.set_page_config(page_title="Predicción de Demanda", layout="centered")
 
 # Título y descripción
 st.title("📦 Predicción de Demanda")
 st.write("Predice la demanda futura de tus productos rápidamente.")
 
-# Input de frecuencia
-dfreq = st.selectbox(
-    "Selecciona frecuencia de tus datos:",
-    options=["Diaria", "Semanal", "Mensual"]
-)
+st.markdown("""
+    <h1 style='font-size:32px;'>Sube tu CSV de ventas</h1>
+""", unsafe_allow_html=True)
+
 
 # Para que no salga error
 max_date = datetime.today().date()
 
 # Carga de archivo CSV
-uploaded_file = st.file_uploader("Sube tu CSV de ventas", type=["csv"] )
+uploaded_file = st.file_uploader("", type=["csv"] )
+
+
+# Definición de columnas
+date_col = st.text_input("Columna de FECHA (nombre EXACTO):")
+target_col = st.text_input("Columna de UNIDADES vendidas (nombre EXACTO):")
+
 # Verificar si el archivo fue cargado
 if uploaded_file is not None:
     # Leer el archivo como DataFrame de Pandas
     df = pd.read_csv(uploaded_file)
 
-    # Mostrar el DataFrame en la aplicación
-    st.write("Contenido del archivo CSV:")
-    st.dataframe(df)
-
-# Definición de columnas
-date_col = st.text_input("Columna de FECHA (nombre EXACTO):")
-target_col = st.text_input("Columna de UNIDADES vendidas (nombre EXACTO):")
+    if date_col not in df.columns or target_col not in df.columns:
+        st.write("Revisa que hayas escrito el nombre exacto de cada columna")
+    else:
+        # Mostrar el DataFrame en la aplicación
+        st.write("Contenido del archivo CSV:")
+        st.dataframe(df[[date_col, target_col]])
 
 # Mostrar mínimo y máximo de fechas una vez definidos
 # df = None
@@ -57,6 +60,7 @@ iforecast = st.date_input(
 #else:
 #    st.write("ERROR: Asegurate de ingresar una fecha maxima de prediccion mayor a la ultima fecha ingresada")
 # -----------------------------------------------------------------------------
+
 
 # Boton para prediccion:
 if st.button("Estimar demanda"):
@@ -90,27 +94,40 @@ if st.button("Estimar demanda"):
     # Título de la aplicación
     st.title('Demanda actual y Predicción de demanda')
 
-    # Crear la gráfica 
+    # Crear la gráfica
+
+    ULTIMOS_DIAS_DATOS = 60
+
+
     grafica_df = pd.DataFrame({
-        'fecha': list(df[date_col]) + list(date_x_test),
-        'valor': list(df[target_col]) + list(predicciones),
-        'tipo': (['Real'] * len(df)) + (['Predicción'] * len(predicciones))
+        'fecha': list(df[date_col][-ULTIMOS_DIAS_DATOS:]) + list(date_x_test),
+        'valor': list(df[target_col][-ULTIMOS_DIAS_DATOS:]) + list(predicciones),
+        'tipo': (['Real'] * (ULTIMOS_DIAS_DATOS-1)) + (['Predicción'] * (len(predicciones)+1))
     })
 
 
-    # Tomar los ultimos 30 dias de la grafica
+    grafica_df['fecha'] = pd.to_datetime(grafica_df['fecha'])
     grafica_df['valor'] = grafica_df['valor'].astype(int)
-
-    # Configurarlo para que Streamlit lo entienda
     grafica_df = grafica_df.sort_values('fecha')
+
+    st.write("Datos de Predicción:")
     st.write(grafica_df[grafica_df['tipo']=='Predicción'])
 
-    # Graficar usando st.line_chart agrupando por tipo
-    st.line_chart(
-        data=grafica_df,
+    # Crear el gráfico con Altair
+    chart = alt.Chart(grafica_df).mark_line().encode(
         x='fecha',
         y='valor',
-        color='tipo'
+        color=alt.Color('tipo', scale=alt.Scale(domain=['Real', 'Predicción'], range=['lightblue', 'red'])),
+        tooltip=['fecha', 'valor', 'tipo']
+    ).properties(
+        title='Pronóstico de Demanda'
     )
 
+    # Mostrar el gráfico en Streamlit
+    st.altair_chart(chart, use_container_width=True)
 
+    # Calcular la suma total de las predicciones
+    total_predicciones = int(sum(predicciones))
+
+    # Mostrar la cifra total debajo de la gráfica
+    st.subheader(f"Cantidad de productos necesarios para los {len(predicciones)} Días: {total_predicciones}")
